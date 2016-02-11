2036be75f5fd0ade47c970e02223475ba1a10bff
Dave Rael <dave@raelyard.com>
2016-02-11 06:45:50 -0700

    Added keyboard navigation to search results

--- assets/themes/bootstrap-3/scripts/search.js
+++ assets/themes/bootstrap-3/scripts/search.js
@@ -1,23 +1,23 @@
 function noResultsPage() {
   var searchResultsContainer = document.getElementsByClassName('search-result-container')[0];
-  var searchResultsList = searchResultsContainer.getElementsByClassName("search-result")[0];
+  var searchResultsList = searchResultsContainer.getElementsByClassName('search-result')[0];
   while (searchResultsList.firstChild) {
     searchResultsList.removeChild(searchResultsList.firstChild);
   }
   searchResultsContainer.classList.add('shown')
-  var emptyListNotification = document.createElement("li");
-  emptyListNotification.innerText = "No results found";
+  var emptyListNotification = document.createElement('li');
+  emptyListNotification.innerText = 'No results found';
   searchResultsList.appendChild(emptyListNotification);
 }
 
 function hideResultsPage() {
   var searchResultsContainer = document.getElementsByClassName('search-result-container')[0];
-  var searchResultsList = searchResultsContainer.getElementsByClassName("search-result")[0];
+  var searchResultsList = searchResultsContainer.getElementsByClassName('search-result')[0];
   searchResultsContainer.classList.remove('shown')
   while (searchResultsList.firstChild) {
     searchResultsList.removeChild(searchResultsList.firstChild);
   }
-  searchResultsContainer.classList.add("shown")
+  searchResultsContainer.classList.add('shown')
 }
 
 function filterPosts(data, query) {
@@ -28,17 +28,26 @@ function filterPosts(data, query) {
 }
 
 function layoutResultsPage(posts) {
-  var searchResultsContainer = document.getElementsByClassName("search-result-container")[0];
-  var searchResultsList = searchResultsContainer.getElementsByClassName("search-result")[0];
+  var searchResultsContainer = document.getElementsByClassName('search-result-container')[0];
+  var searchResultsList = searchResultsContainer.getElementsByClassName('search-result')[0];
   while (searchResultsList.firstChild) {
     searchResultsList.removeChild(searchResultsList.firstChild);
   }
-  searchResultsContainer.classList.add("shown")
+  searchResultsContainer.classList.add('shown')
   for(var i=0;i<posts.length;++i) {
-    var link = document.createElement("a");
-    var post = document.createElement("li");
+    var link = document.createElement('a');
+    var image = document.createElement('img');
+    var post = document.createElement('li');
     link.href = posts[i].url;
     link.innerText = posts[i].title;
+    image.src = posts[i].image;
+    image.classList.add('img');
+    image.classList.add('img-circle');
+    image.classList.add('search-result-image');
+    if(i===0) {
+      post.classList.add('selected');
+    }
+    post.appendChild(image);
     post.appendChild(link);
     searchResultsList.appendChild(post);
   }
@@ -47,7 +56,7 @@ function layoutResultsPage(posts) {
 var searchBox = document.getElementById('search')
 searchBox.addEventListener('input', function() {
   var query = searchBox.value;
-  if (query !== null) {
+  if (query !== null && query.length > 0) {
     $.getJSON('/search.json', function(data) {
       posts = filterPosts(data, query);
       if(posts == null) {
@@ -60,4 +69,68 @@ searchBox.addEventListener('input', function() {
       }
     });
   }
+  else {
+    hideResultsPage();
+  }
 });
+
+searchBox.addEventListener('keydown', function(event) {
+  if(event.keyCode === 27) {
+    searchBox.value = '';
+  }
+  var moveSelection = false;
+  var selectionMoveUp = false;
+  if(event.keyCode == 38) {
+    moveSelection = true;
+    selectionMoveUp = true;
+  }
+  if(event.keyCode == 40) {
+    moveSelection = true;
+  }
+  if(event.keyCode == 13) {
+    event.preventDefault();
+    window.location = currentSelection().getElementsByTagName('a')[0].href;
+    return false;
+  }
+  if(moveSelection){
+    if(selectionMoveUp){
+      selectPreviousResult();
+    }
+    else {
+      selectNextResult();
+    }
+  }
+});
+
+function selectPreviousResult() {
+  selectResult(function(i){ return i - 1});
+}
+
+function selectNextResult() {
+  selectResult(function(i){ return i + 1});
+}
+
+function currentSelection(searchResultsList) {
+  if(!searchResultsList) {
+    searchResultsList = document.getElementsByClassName('search-result')[0];
+  }
+  return searchResultsList.getElementsByClassName('selected')[0];
+}
+
+function currentSelectionIndex() {
+  var searchResultsList = document.getElementsByClassName('search-result')[0];
+  return [].slice.call(searchResultsList.children).indexOf(currentSelection(searchResultsList));
+}
+
+function selectResult(selectionMovementFunction) {
+  var searchResultsList = document.getElementsByClassName('search-result')[0];
+  var i = selectionMovementFunction(currentSelectionIndex());
+  if(i < 0) {
+    i = searchResultsList.children.length - 1;
+  }
+  if(i >= searchResultsList.children.length) {
+    i = 0;
+  }
+  currentSelection(searchResultsList).classList.remove('selected');
+  searchResultsList.children[i].classList.add('selected');
+}
