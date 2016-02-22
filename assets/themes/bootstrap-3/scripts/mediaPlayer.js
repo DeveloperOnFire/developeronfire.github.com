01daf9768f8f1c25122ebacc41dc3ff29659c4ee
Dave Rael <dave@raelyard.com>
2016-02-21 20:25:10 -0700

    Added script to make clicking chapters work
    to jump the audio player to that chapter of the show

---
+++ assets/themes/bootstrap-3/scripts/mediaPlayer.js
@@ -0,0 +1,39 @@
+function audioPlayer(){
+  return document.getElementById('episode-audio');
+}
+
+function playPause() {
+  self.isPlaying() ? player.pause() : player.play();
+}
+
+function play(time) {
+  audioPlayer().play();
+}
+
+function jumpAudioToTime(time) {
+  audioPlayer().currentTime = time;
+  play();
+}
+
+function parseTimeToSeconds(time) {
+  var timeParts = time.split(':');
+  var seconds = 0;
+  for (var i = 0; i < timeParts.length; ++i) {
+    var timePartPower = timeParts.length - i - 1;
+    seconds += timeParts[i] * (Math.pow(60,timePartPower));
+  }
+  return seconds;
+}
+
+(function init(){
+  console.log('entering init');
+  var jumpAudioLinks = document.getElementsByClassName('chapter-timestamp');
+  for(var i = 0;i<jumpAudioLinks.length; ++i) {
+    (function(timeElement) {
+      timeElement.parentElement.addEventListener('click', function() {
+        var time = parseTimeToSeconds(timeElement.innerHTML);
+        jumpAudioToTime(time);
+      });
+    })(jumpAudioLinks[i]);
+  }
+})();
